647ac5da2206885ecd2e46a4e01f8f1e9ef5b430
Juan Jimeno <jimenojmm@gmail.com>
2017-03-16 09:58:47 +0800

    cleaned up

--- install_ros_desktop.sh
+++ install_ros_desktop.sh
@@ -14,6 +14,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 
 #Setup the keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
 #ROS Installation
 sudo apt-get -y  update
 sudo apt-get -y install ros-kinetic-desktop-full
