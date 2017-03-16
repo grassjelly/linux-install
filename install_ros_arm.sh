26b6ebac45373eabbf63d78433fc033042304d09
Juan Jimeno <jimenojmm@gmail.com>
2017-03-16 10:01:08 +0800

    updated ROS installation keys

--- install_ros_arm.sh
+++ install_ros_arm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #Check if user has  configured Ubuntu repositories(ROS ARM Installation Section 2.1"
 echo ""
-echo "Have you configured your Ubuntu repositories?" 
+echo "Have you configured your Ubuntu repositories?"
 echo "You must allow \"restricted\", \"universe\" , \"multiverse\"."
 echo "https://help.ubuntu.com/community/Repositories/Ubuntu"
 echo ""
@@ -14,7 +14,7 @@ sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
 #Setup the sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 #Setup the keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 #ROS Installation
 sudo apt-get -y  update
@@ -40,6 +40,5 @@ source ~/.bashrc
 sudo apt-get -y install build-essential
 
 echo ""
-echo "ROS Installation Done!" 
+echo "ROS Installation Done!"
 echo "You can create your catkin workspace now. https://wiki.ros.org/catkin/Tutorials/create_a_workspace"
-
